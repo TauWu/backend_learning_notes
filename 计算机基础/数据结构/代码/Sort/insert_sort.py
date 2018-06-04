9fd0f4c15bb7d8e5dbea8a536f106fbfdb196c33
TauWoo <tauwoo@seuxw.cc>
2018-06-04 19:02:48 +0800

    插入排序代码部分提交

--- 计算机基础/数据结构/代码/Sort/insert_sort.py
+++ 计算机基础/数据结构/代码/Sort/insert_sort.py
@@ -38,7 +38,47 @@ def insert_sort(unsorted_list):
         unsorted_list[:i+1] = insert_once(unsorted_list[:i], unsorted_list[i])
     return unsorted_list
 
+def insert_once_half(sorted_list, elem):
+    '''insert_once_half
+    插入排序的一次插入操作
+    params:
+        sorted_list：已排序列表
+        elem：待插入元素
+    returns:
+        sorted_list：已排序列表
+    '''
+    low = 0
+    high = len(sorted_list) - 1
+    flag = True
+
+    while flag:
+        print(high, low)
+        m = int((high - low) / 2)
+        if m <= 0:
+            break
+        if sorted_list[m] > elem:
+            high = m
+            continue
+        else:
+            low = m
+            continue
+        
+    print("DEBUG:", sorted_list)
+    return sorted_list
+
+def insert_sort_half(unsorted_list):
+    '''insert_sort
+    插入排序操作
+    params：
+        unsorted_list：未排序序列
+    returns:
+        sorted_list:已排序序列
+    '''
+    for i in range(1, len(unsorted_list)):
+        unsorted_list[:i+1] = insert_once_half(unsorted_list[:i], unsorted_list[i])
+    return unsorted_list
+
 if __name__ == "__main__":
     print(
-        insert_sort([6,4,3,12,32,3,12,43,12,4,21,5,1])
+        insert_once_half([1,2,3,4,5,6,7,8,9,10,11,12,13], 7)
     )
