1285f04fd879d8086b95e714ca775ac789d98ac4
TauWoo <tauwoo@seuxw.cc>
2018-06-05 14:33:48 +0800

    希尔排序代码I

---
+++ 计算机基础/数据结构/代码/Sort/shell_sort.py
@@ -0,0 +1,29 @@
+# shell_sort.py
+#
+# Shell Sort 希尔排序算法
+# Author:   Tau
+# Date:     2018-06-05
+#
+
+def shell_sort_once(unsorted_list, add):
+    '''shell_sort_once
+    params:
+        unsorted_list: 未排序列表
+        add：本趟增量
+    returns:
+        unsorted_list：本轮返回结果
+    '''
+    pass
+
+def shell_sort(unsorted_list, add_list):
+    '''shell_sort
+    params：
+        unsorted_list: 未排序列表
+        add_list：增量列表
+    returns:
+        sorted_list：排序列表
+    '''
+    pass
+
+if __name__ == "__main__":
+    shell_sort([0,4,1,6,6,8,1,16,27,18,7,2,22,1,32], [5,3,1])
