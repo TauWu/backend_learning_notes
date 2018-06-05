463bd2fb22aba7b8e361e10702b26175c24217ef
TauWoo <tauwoo@seuxw.cc>
2018-06-05 15:16:09 +0800

    希尔排序代码C

--- 计算机基础/数据结构/代码/Sort/shell_sort.py
+++ 计算机基础/数据结构/代码/Sort/shell_sort.py
@@ -5,6 +5,8 @@
 # Date:     2018-06-05
 #
 
+from insert_sort import insert_sort
+
 def shell_sort_once(unsorted_list, add):
     '''shell_sort_once
     params:
@@ -13,7 +15,10 @@ def shell_sort_once(unsorted_list, add):
     returns:
         unsorted_list：本轮返回结果
     '''
-    pass
+    for i in range(add):
+        unsorted_list[i::add] = insert_sort(unsorted_list[i::add])
+        print("SHELL DEBUG:", unsorted_list)
+    return unsorted_list
 
 def shell_sort(unsorted_list, add_list):
     '''shell_sort
@@ -23,7 +28,10 @@ def shell_sort(unsorted_list, add_list):
     returns:
         sorted_list：排序列表
     '''
-    pass
+    for add in add_list:
+        unsorted_list = shell_sort_once(unsorted_list, add)
+        
+    return unsorted_list
 
 if __name__ == "__main__":
-    shell_sort([0,4,1,6,6,8,1,16,27,18,7,2,22,1,32], [5,3,1])
+    shell_sort([0,4,1,6,6,8,1,16,27,18,7,2,22,1,32], [6,3,1])
