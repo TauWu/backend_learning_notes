3e7166ede5facb9f7c9c6e0b6b5b19d96dfbb2bd
TauWoo <tauwoo@seuxw.cc>
2018-09-19 18:54:02 +0800

    Add new_ubuntu install shell.

---
+++ issues/装机/new_ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Update and upgrade.
+sudo apt-get update
+sudp apt-get upgrade
+
+# Install softwares.
+
+## Coding language.
+sudo apt-get install python
+sudo apt-get install python3
+sudo apt-get install python-pip
+sudo apt-get install python3-pip
+
+sudo apt-get install golang
+sudo apt-get install mysql-server
+sudo apt-get install mysql-client
+sudo apt-get install redis-server
+sudo apt-get install redis-client
+
+## Coding softwares.
+sudo apt-get install vscode
+sudo apt-get install git
+sudo apt-get install tig
+sudo apt-get install htop
+sudo apt-get install mysql-workbench
+sudo apt-get install redis-desktop-manager
+sudo apt-get install wireshark
+sudo apt-get install postman
+sudo apt-get install charles
+sudo apt-get install remmina
+sudo apt-get install teamviewer
